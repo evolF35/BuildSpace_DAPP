7fb438009163c8e990ffd94b24c6a4a61b15cc11
evolF35 <maccyair@gmail.com>
2022-11-12 22:27:24 -0800

    testnett

--- wave_portal/hardhat.config.js
+++ wave_portal/hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require('dotenv').config()
+
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -8,8 +11,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const GoerliUrl =
+   process.env.ALCHEMY_API_KEY ?
+      `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
+      process.env.OPTIMISM_GOERLI_URL
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    "goerli": {
+       url: GoerliUrl,
+       accounts: { mnemonic: process.env.MNEMONIC }
+    }
+  }
 };
