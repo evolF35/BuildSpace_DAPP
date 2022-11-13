3fa989bf46acf13e366c6fbf3e85dde78b33f87e
evolF35 <maccyair@gmail.com>
2022-11-12 21:54:08 -0800

    get going

--- wave_portal/contracts/WavePortal.sol
+++ wave_portal/contracts/WavePortal.sol
@@ -5,7 +5,33 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint256 totalWaves;
+
+    mapping (address => uint256) public wavooors;
+
     constructor(){
         console.log("JS in Solidity?");
     }
+
+    function wave() public {
+        totalWaves = totalWaves + 1;
+        addwave();
+        console.log("%s has waved!",msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %d total waves!", totalWaves);
+        return(totalWaves);
+    }
+
+    function addwave() private {
+        wavooors[msg.sender] += 1;
+    }
+
+    function AddrWaveBalance() public view returns (uint256) {
+        console.log("%s has %d total waves!", msg.sender , wavooors[msg.sender]);
+        return (wavooors[msg.sender]);
+    }
+
 }
--- wave_portal/scripts/run.js
+++ wave_portal/scripts/run.js
@@ -1,12 +1,31 @@
 
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
-    
     await waveContract.deployed();
 
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    await waveContract.getTotalWaves();
+
+    const waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    await waveContract.getTotalWaves();
+
+
+    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+    await secondWaveTxn.wait();
+    const WecondWaveTxn = await waveContract.connect(randomPerson).wave();
+    await WecondWaveTxn.wait();
+
+    let td2 = await waveContract.connect(randomPerson).AddrWaveBalance();
+
+    await waveContract.getTotalWaves();
 }
 
 
